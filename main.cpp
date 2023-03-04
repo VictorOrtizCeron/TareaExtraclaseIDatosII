35319906386a71c3a5cf37eb601b6785224ccc82
VICTOR ORTIZ <vortiz@estudiantec.cr>
2023-03-04 12:09:33 -0600

    Setters y getters de clase node implementados

--- main.cpp
+++ main.cpp
@@ -3,14 +3,42 @@ using namespace std;
 
 
 class Node {
-public:
+private:
     int valor;
     Node* NextPTR;
-    
+public:
+    int getValor(){
+        return valor;
+    }
+    void setValor(int num){
+        valor = num; 
+    }
+    Node* getNext(){
+        return NextPTR;
+    }
+    void setNext(Node* Next){
+        NextPTR = Next;
+    }
 };
+
+void printList(Node*n){
+    while(n!= NULL){
+        cout<< n->getValor()<< endl;
+    }
+}
 
 int main(){
 
+    Node*head = new Node();
+    Node*second = new Node();
+    Node*third = new Node();
+
+    head->setValor(1); 
+    head -> set
+    second->setValor(2);
+    third -> setValor(3);
+
+    printList(head);
     cout << "ay lmaoo";
     return 0;
 }
