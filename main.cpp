6e4b7bcb77150ce64b51e55aee24e7f13445e8fb
sebas <sebhernandez@estudiantec.cr>
2023-03-06 12:10:26 -0600

    function insertar lista

--- main.cpp
+++ main.cpp
@@ -16,12 +16,34 @@ void printList(Node*n){
     }
 }
 
-void xd(int){
+class List{
 
-}
+public: 
+    void insertarLista(Node* &lista, int n){
+        Node* nuevo_nodo = new Node();
+        nuevo_nodo -> valor = n;
+
+        Node* aux1 = lista;
+        Node* aux2;
+
+        while((aux1 != NULL) && (aux1 -> valor < n)){
+            aux2 = aux1;
+            aux1 = aux1 -> NextPTR;
+        }
+        //Este while y condicional es simplemente para que la lista quede en orden de valor creciente.
+        if(lista == aux1){
+            lista = nuevo_nodo;
+        }else{
+            aux2 -> NextPTR = nuevo_nodo;
+        }
+        nuevo_nodo -> NextPTR = aux1;
+    }
+
+};
 
 int main(){
 
+    Node*lista = NULL;
     Node*head = new Node();
     Node*second = new Node();
     Node*third = new Node();
