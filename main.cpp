c5b40213ae4ab99bd21bfed217ecd8b61b0c9523
Victor <vortizc@estudiantec.cr>
2023-03-11 16:36:15 -0600

    Cambios a collector y main( )

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ class Node;//Inicialización de clase node que previene un problema de dependenc
 //Clase de nodos de Collector
 class collectorNode{
     public:
-        Node * NodePTR;
+        void * NodePTR;
         collectorNode * NextCollectorPTR;
 
-    collectorNode(Node* DeletedNodePTR){
+    collectorNode(void* DeletedNodePTR){
 
         this -> NodePTR = DeletedNodePTR;
-        this -> NextCollectorPTR = nullptr;
+        this -> NextCollectorPTR = NULL;
     }
 };
 //Clase de lista Collector
@@ -28,7 +28,8 @@ public:
         size = 0;
     }
 
-    void insertFirst( Node* DeletedNodePTR){
+    void insertFirst( void* DeletedNodePTR){
+        
         collectorNode* newNode = new collectorNode(DeletedNodePTR);
         newNode -> NextCollectorPTR = head;
         head = newNode;
@@ -37,7 +38,7 @@ public:
 
     void deleteFirst(){
         if(head != nullptr){
-
+            cout<< "delete working"<<endl;
             collectorNode* Temp = head;
             head = head -> NextCollectorPTR;
             delete Temp;
@@ -47,6 +48,9 @@ public:
 
 };
 //Clase Node para lista de ints
+
+collectorList Collector;
+
 class Node {
 
 public:
@@ -57,12 +61,33 @@ public:
         this->valor= valor;
         this->NextPTR = nullptr;
     }
+
     //Sobrecarga del método New para el Nodo regular. Aqui se debe validar el caso del nuevo nodo y asignar la memoria correspondiente
-    void* operator new(size_t size, collectorList *Collector) {
-        void* p = ::operator new(size);
+    void* operator new(size_t size) {
+        //Si collector.size == o ; no mames malloc. Else, asignar de collector.
+        /**
+         * if(Collector.size==0){
+         *  automatico facilongo
+         * }
+         * else{
+         *  void * p = collector.head;
+         *  Collector.deleteFirst();
+         *  return p;
+         * }
+         * 
+        */
+        void* p = malloc(sizeof(int*));
         std::cout << "Custom new called, size = " << size << ", pointer = " << p << std::endl;
-        printf("%p \n", Collector -> head);
+        cout<< Collector.size <<endl;
         return p;
+    }
+
+    void operator delete(void* p){
+        
+        Collector.insertFirst(p);
+        cout << "Custom delete called, pointer = " << p << endl;
+        //meter en collector xD
+        ::operator delete(p);
     }
 };
 // Clase de List
@@ -79,8 +104,8 @@ public:
         size = 0;
     }
 
-    void insertFirst(int valor , collectorList* Collector){
-        Node* newNode = new (Collector) Node(valor);
+    void insertFirst(int valor){
+        Node* newNode = new  Node(valor);
         newNode -> NextPTR = head;
         head = newNode;
         size ++;
@@ -88,7 +113,7 @@ public:
 
     void deleteFirst(){
         if(head != nullptr){
-
+            //Node* Temp = new (Collector) Node(head->valor);
             Node* Temp = head;
             head = head -> NextPTR;
             delete Temp;
@@ -113,29 +138,45 @@ void printCollectorList(collectorNode*n){
 }
 //Main de pruebas
 int main(){
+    cout<< "comienza"<<endl;
+    //List *newList = new List();//crea nueva lista
+    void * prueba ;
 
-    List *newList = new List();//crea nueva lista
+    void * prueba2 ;
 
-    collectorList*Collector = new collectorList();//crea nueva lista collector
-    Node * Prueba = new (Collector) Node(5);//Crea un puntero de nodo  para pruebas únicamente.
+    Collector.insertFirst(prueba);
+    
+    Collector.insertFirst(prueba2);
 
-    printf("%p\n",Prueba);//imprime dirección del nodo Prueba
-    Collector->insertFirst(Prueba); //inserta puntero del nodo en collector list
+    printCollectorList(Collector.head);
+
+    Collector.deleteFirst();
+
+    printCollectorList(Collector.head);
+
+    Node * Prueba = new Node(5);//Crea un puntero de nodo  para pruebas únicamente.
+
+    delete Prueba;
+
+    //Liberar memoria pls
+
+
+    //printf("%p\n",Prueba);//imprime dirección del nodo Prueba
+    //Collector->insertFirst(Prueba); //inserta puntero del nodo en collector list
     //Inserción de nodos regulares en lista enlazada de ints
-    newList->insertFirst(1,Collector);
-    newList->insertFirst(2,Collector);
-    newList->insertFirst(3,Collector);
+    //newList->insertFirst(1);
+    //newList->insertFirst(2,Collector);
+    //newList->insertFirst(3,Collector);
 
     //print de elementos de lista
-    printList(newList->head);
-    cout << "\n";
+    //printList(newList->head);
+    //cout << "\n";
     //eliminación de primer valor de lista
-    newList ->deleteFirst();
+    //newList ->deleteFirst();
     //comprobación de funcionamiento de deleteFirst
-    printList(newList->head);
-
-    //impresión de punteros de lista collector (Nil significa lista vacía :D )
-    printCollectorList(Collector->head);
+    //printList(newList->head);
+    //impresión de punteros de lista collector (Nil significa lista Collector vacía :D )
+    //printCollectorList(Collector->head);
 
     return 0;
 }
